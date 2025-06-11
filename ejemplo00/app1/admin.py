f068e54f9ea0300dd8dd5bcacd0836b50d9accd7
Omer21200 <omerbenitez212000@gmail.com>
2025-06-11 08:48:35 -0500

    Ciudad

--- ejemplo00/app1/admin.py
+++ ejemplo00/app1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# Importar las clases del modelo
+from app1.models import Estudiante
+from app1.models import Ciudad
+
+admin.site.register(Estudiante)
+admin.site.register(Ciudad)
